4a6470ac31626f15bb75344f506491e441ba6437
ShreyasAyyengar <shreyas.ayyengar@gmail.com>
2024-06-19 10:43:49 +0800

    Added support for glowing items.

--- src/main/java/dev/shreyasayyengar/menuapi/menu/MenuItem.java
+++ src/main/java/dev/shreyasayyengar/menuapi/menu/MenuItem.java
@@ -5,6 +5,8 @@ import dev.shreyasayyengar.menuapi.action.MenuItemClickAction;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -164,6 +166,18 @@ public class MenuItem {
         modifiedMeta.setOwningPlayer(player);
         item.setItemMeta(modifiedMeta);
 
+        return this;
+    }
+
+    /**
+     * Adds an enchantment glint texture to this MenuItem. Calls {@link #updateItemMeta()}.
+     *
+     * @return This MenuItem.
+     */
+    public MenuItem withEnchantmentGlint() {
+        this.meta.addEnchant(Enchantment.DURABILITY, 1, true);
+        this.meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        updateItemMeta();
         return this;
     }
 
